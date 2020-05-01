3c96ae3df2cc0f21c592fbc602a56b651c7be1b3
srijanar <srijana@facialstats.com>
2020-05-01 16:47:53 -0400

    made root url common

--- src/Controller/AppData.php
+++ src/Controller/AppData.php
@@ -1,8 +1,14 @@
 <?php
-require_once ('../config/db_connection.php');
+require_once ($_SERVER['DOCUMENT_ROOT'].'/gmd/config/db_connection.php');
 function getUsers()
 {
     $mysqli = getDatabaseConnection();
     $result = $mysqli->query("SELECT id, CONCAT(name,lastname) as Name FROM user");
+    return $result;
+}
+
+function getCameras(){
+    $mysqli = getDatabaseConnection();
+    $result = $mysqli->query("SELECT * FROM camera");
     return $result;
 }
