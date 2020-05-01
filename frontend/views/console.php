fa1af68e7ad22d0ebed2be68be58df376c5d0fed
srijanar <srijana@facialstats.com>
2020-04-30 22:40:19 -0400

    added console page

---
+++ frontend/views/console.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+    <link href="https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/material-components-web@v4.0.0/dist/material-components-web.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="../frontend/assets/css/dashboard.css" rel="stylesheet">
+</head>
+
+<body>
+<header class="mdc-top-app-bar app-header" style="top:0">
+    <div class="mdc-top-app-bar__row">
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <span class="mdc-top-app-bar__title">Welcome to console</span>
+        </section>
+        <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end">
+            <button class="material-icons mdc-top-app-bar__navigation-icon mdc-icon-button">menu</button>
+        </section>
+    </div>
+</header>
+</body>
+
+</html>
